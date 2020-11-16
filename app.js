6c0a16cb7dd9d04069a5e764d106591e8a009b0c
Sujitraj Thirumurthy <sujitrajt@gmail.com>
2020-11-16 19:40:56 +0530

    authentication using passport

--- app.js
+++ app.js
@@ -11,55 +11,8 @@ var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
 var session = require("express-session");
 var FileStore = require("session-file-store")(session);
-var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(cookieParser("12345-67890-09876-54321"));
-
-app.use(
-  session({
-    name: "session-id",
-    secret: "12345-67890-09876-54321",
-    saveUninitialized: false,
-    resave: false,
-    store: new FileStore()
-  })
-);
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-function auth(req, res, next) {
-  console.log(req.session);
-
-  if (!req.session.user) {
-    var err = new Error("You are not authenticated!");
-    err.status = 403;
-    return next(err);
-  } else {
-    if (req.session.user === "authenticated") {
-      next();
-    } else {
-      var err = new Error("You are not authenticated!");
-      err.status = 403;
-      return next(err);
-    }
-  }
-}
-app.use(auth);
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/dishes", dishRouter);
-app.use("/promotions", promoRouter);
-app.use("/leaders", leaderRouter);
-
+var passport = require("passport");
+var authenticate = require("./authenticate");
 const mongoose = require("mongoose");
 const Dishes = require("./models/dishes");
 const url = "mongodb://localhost:27017/conFusion";
@@ -72,6 +25,53 @@ connect.then(
     console.log(err);
   }
 );
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use(cookieParser("12345-67890-09876-54321"));
+
+app.use(
+  session({
+    name: "session-id",
+    secret: "12345-67890-09876-54321",
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+function auth(req, res, next) {
+  console.log(req.user);
+
+  if (!req.user) {
+    var err = new Error("You are not authenticated!");
+    err.status = 403;
+    next(err);
+  } else {
+    next();
+  }
+}
+app.use(auth);
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.use("/", indexRouter);
+// app.use("/users", usersRouter);
+app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
